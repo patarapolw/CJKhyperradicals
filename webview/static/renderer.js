8e282cce933b028fd4798c9e408e5dbb62ef6884
patarapolw <patarapolw@gmail.com>
2018-04-15 12:08:22 +0700

    make it better

--- webview/static/renderer.js
+++ webview/static/renderer.js
@@ -94,21 +94,25 @@ function speak(vocab, lang){
         data: {
             'vocab': vocab,
             'lang': lang
-        },
-        dataType: 'arraybuffer'
+        }
     }).done(function(response){
         if (!(location.hostname === "localhost" || location.hostname === "127.0.0.1"))
         {
-            var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-            var source = audioCtx.createBufferSource();
-            audioCtx.decodeAudioData(
-                response,
-                function (buffer) {
-                    source.buffer = buffer;
-                    source.connect(audioCtx.destination);
-                    source.loop = false;
-                });
-            source.start(0);
+            // read base64
+            var snd = new Audio("data:audio/x-mp3;base64," + response);
+            snd.play();
+
+            // read byteArray
+//            var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+//            var source = audioCtx.createBufferSource();
+//            audioCtx.decodeAudioData(
+//                response,
+//                function (buffer) {
+//                    source.buffer = buffer;
+//                    source.connect(audioCtx.destination);
+//                    source.loop = false;
+//                });
+//            source.start(0);
         }
     })
 }
